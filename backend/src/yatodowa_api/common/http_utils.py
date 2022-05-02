1883bb8a6ca96e70adbfeca1b8bb1317dd20d2f8
amine4567 <souikiamine@gmail.com>
2022-05-02 20:31:16 +0000

    feat: implement a custom flask Blueprint called 'ValidatedBlueprint' to automatically validate data using pydantic when decorating with the 'route' method

--- backend/src/yatodowa_api/common/http_utils.py
+++ backend/src/yatodowa_api/common/http_utils.py
@@ -1,26 +1,62 @@
+import inspect
 from functools import wraps
 from typing import Callable
 
 import pydantic
-from flask import jsonify, request
+from flask import Blueprint, jsonify, request
+from yatodowa_api.common.schemas import APICallError, ErrorType
 from yatodowa_api.consts import REQUEST_ARGS_KWARG, REQUEST_BODY_KWARG
 
 
+def serialize_response(decorated_f: Callable) -> Callable:
+    @wraps(decorated_f)
+    def modified_f(*args, **kwargs):
+        response, response_code = decorated_f(*args, **kwargs)
+        # TODO: assert on reponse type
+        if isinstance(response, list):
+            return jsonify([elt.dict() for elt in response]), response_code
+        else:
+            return jsonify(response.dict()), response_code
+
+    return modified_f
+
+
 def validate_url_vars(decorated_f: Callable) -> Callable:
+    """decorator to validate an URL variable as defined by flask using pydantic
+    Example: http://127.0.0.1:5000/api/v1/tasks/408bce1f-f36e-4d39-9d69-614071572d84
+
+    Args:
+        decorated_f (Callable): _description_
+
+    Returns:
+        Callable: _description_
+    """
+    # TODO: remove check on REQUEST_ARGS_KWARG and REQUEST_BODY_KWARG
     @wraps(decorated_f)
     def modified_f(*args, **kwargs):
         try:
             validated_f = pydantic.validate_arguments(decorated_f)
             validated_f.validate(*args, **kwargs)
         except pydantic.ValidationError as e:
-            return jsonify({"validation_error": e.errors()}), 400
+            print(e.errors())
+            return (
+                [
+                    APICallError(
+                        type=ErrorType.VALIDATION,
+                        subtype=error["type"],
+                        message=".".join(error["loc"]) + ": " + error["msg"],
+                    )
+                    for error in e.errors()
+                ],
+                400,
+            )
 
         return validated_f(*args, **kwargs)
 
     return modified_f
 
 
-def validate_request_vals(kwarg_name: str, request_vals_getter: Callable):
+def validate_request_vals(kwarg_name: str, request_vals_getter: Callable) -> Callable:
     def decorator(decorated_f: Callable) -> Callable:
         @wraps(decorated_f)
         def modified_f(*args, **kwargs):
@@ -42,7 +78,17 @@ def validate_request_vals(kwarg_name: str, request_vals_getter: Callable):
             try:
                 request_args = Schema(**request_vals_getter())
             except pydantic.ValidationError as e:
-                return jsonify({"validation_error": e.errors()}), 400
+                return (
+                    [
+                        APICallError(
+                            type=ErrorType.VALIDATION,
+                            subtype=error["type"],
+                            message=".".join(error["loc"]) + ": " + error["msg"],
+                        )
+                        for error in e.errors()
+                    ],
+                    400,
+                )
 
             new_kwargs = {**kwargs, kwarg_name: request_args}
             return decorated_f(*args, **new_kwargs)
@@ -56,3 +102,28 @@ validate_request_body = validate_request_vals(
     REQUEST_BODY_KWARG, lambda: request.get_json()
 )
 validate_request_args = validate_request_vals(REQUEST_ARGS_KWARG, lambda: request.args)
+
+
+class ValidatedBlueprint(Blueprint):
+    def route(self, *args, **kwargs):
+        # TODO: add validate_request_vals
+        def decorator(decorated_f: Callable) -> Callable:
+            f_params = inspect.signature(decorated_f).parameters
+
+            modified_f = decorated_f
+
+            modified_f = validate_url_vars(modified_f)
+
+            if REQUEST_ARGS_KWARG in f_params:
+                modified_f = validate_request_args(modified_f)
+
+            if REQUEST_BODY_KWARG in f_params:
+                modified_f = validate_request_body(modified_f)
+
+            modified_f = serialize_response(modified_f)
+
+            modified_f = super(Blueprint, self).route(*args, **kwargs)(modified_f)
+
+            return modified_f
+
+        return decorator
--- backend/src/yatodowa_api/components/tasks/controller.py
+++ backend/src/yatodowa_api/components/tasks/controller.py
@@ -1,46 +1,42 @@
 from uuid import UUID
 
 import yatodowa_api.components.tasks.service as TaskService
-from flask import Blueprint, jsonify
-from yatodowa_api.common.http_utils import (
-    validate_request_args,
-    validate_request_body,
-    validate_url_vars,
-)
+from yatodowa_api.common.http_utils import ValidatedBlueprint
+from yatodowa_api.common.schemas import APICallError, ErrorType
 from yatodowa_api.components.collections.exceptions import CollectionNotFoundError
 from yatodowa_api.components.tasks.exceptions import TaskNotFoundError
 from yatodowa_api.consts import COMMON_API_ENDPOINT
 
 from .schemas import TaskQueryArgs, TaskQueryBody
 
-tasks_api = Blueprint("tasks_api", __name__)
+tasks_api = ValidatedBlueprint("tasks_api", __name__)
 
 
 @tasks_api.route(COMMON_API_ENDPOINT + "/tasks", methods=["GET"])
-@validate_request_args
 def get_tasks(request_args: TaskQueryArgs):
     print(request_args)
     tasks_response = TaskService.get_tasks()
-    return jsonify([elt.dict() for elt in tasks_response]), 200
+    return tasks_response, 200
 
 
 @tasks_api.route(COMMON_API_ENDPOINT + "/tasks", methods=["POST"])
-@validate_request_body
 def add_task(request_body: TaskQueryBody):
     try:
         task_response = TaskService.add_task(request_body)
     except CollectionNotFoundError as e:
-        return jsonify({"error_message": str(e)}), 400
+        return APICallError(type=ErrorType.GENERIC, message=str(e)), 400
     else:
-        return jsonify(task_response.dict()), 201
+        return task_response, 201
 
 
-@tasks_api.route(COMMON_API_ENDPOINT + "/tasks/<task_id>", methods=["DELETE"])
-@validate_url_vars
+@tasks_api.route(
+    COMMON_API_ENDPOINT + "/tasks/<task_id>",
+    methods=["DELETE"],
+)
 def delete_task(task_id: UUID):
     try:
         deleted_task = TaskService.delete_task(task_id)
     except TaskNotFoundError as e:
-        return jsonify({"error_message": str(e)}), 400
+        return APICallError(type=ErrorType.GENERIC, message=str(e)), 400
     else:
-        return jsonify(deleted_task.dict()), 200
+        return deleted_task, 200
